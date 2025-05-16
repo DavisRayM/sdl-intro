b6930bcb0155c38650e6dd01385b8a60e242c792
Davis Muro <git@davisraym.com>
2025-05-15 22:05:46 -0700

    feat: load texture from file

--- 06/render.c
+++ 06/render.c
@@ -1,7 +1,5 @@
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_render.h>
-#include <SDL3/SDL_video.h>
+#include <SDL3_image/SDL_image.h>
 
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
@@ -15,7 +13,8 @@ typedef struct {
   SDL_Texture *texture;
 } AppState;
 
-SDL_Texture *load_texture(const char *path);
+/// Loads texture from a file
+SDL_Texture *load_texture(AppState *as, const char *path);
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   if (!SDL_SetAppMetadata("SDL 2D Rendering", "1.0", "com.davisraym.sdl.2d")) {
@@ -44,15 +43,46 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   }
 
   if (!SDL_SetRenderDrawColor(as->renderer, 0xFF, 0xFF, 0xFF, 0xFF)) {
-    SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "COuldn't set render draw color: %s",
+    SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Couldn't set render draw color: %s",
                  SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
+  SDL_Texture *texture = load_texture(as, "fixtures/texture.png");
+  if (!texture) {
+    SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Couldn't load texture: %s",
+                 SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+  as->texture = texture;
+  *appstate = as;
+
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+  AppState *as = (AppState *)appstate;
+
+  if (!SDL_RenderClear(as->renderer)) {
+    SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Couldn't clear render: %s",
+                 SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
+  if (!SDL_RenderTexture(as->renderer, as->texture, NULL, NULL)) {
+    SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Couldn't render texture: %s",
+                 SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
+  if (!SDL_RenderPresent(as->renderer)) {
+    SDL_LogError(SDL_LOG_CATEGORY_VIDEO,
+                 "Couldn't update application screen: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
   return SDL_APP_CONTINUE;
 }
-
-SDL_AppResult SDL_AppIterate(void *appstate) { return SDL_APP_CONTINUE; }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
   switch (event->type) {
@@ -68,8 +98,20 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
   if (appstate != NULL) {
     AppState *as = (AppState *)appstate;
+
+    SDL_DestroyTexture(as->texture);
     SDL_DestroyWindow(as->window);
     SDL_DestroyRenderer(as->renderer);
     SDL_free(as);
   }
+}
+
+SDL_Texture *load_texture(AppState *as, const char *path) {
+  SDL_Texture *texture = IMG_LoadTexture(as->renderer, path);
+  if (!texture) {
+    SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Couldn't load image %s: %s", path,
+                 SDL_GetError());
+  }
+
+  return texture;
 }
